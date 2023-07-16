0bcbbe9bd4a13bcf76b0656c9dba885255717e01
Vadim <jagabumba@gmail.com>
2023-07-16 14:39:30 +0700

    Task 1

--- 1_1/Program.cs
+++ 1_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.WriteLine("введите четырехзначное число");
+int number = int.Parse(Console.ReadLine());
+int pal = 0, num = number;
+while (number > 0)
+{
+  pal = pal * 10 + number % 10; number /= 10;
+}
+Console.WriteLine(num == pal ? "Да, это палиндром" : "Нет, это не палиндром");
+
